7dd64aa63b999c36f831f61cf6b2999712373ab0
Полина <ankudinovapol@gmail.com>
2022-10-11 14:39:33 +0500

    clear code, remote trash

--- QR-Chemistry-main/app/src/main/java/com/example/shibarichat/ItemActivity.kt
+++ QR-Chemistry-main/app/src/main/java/com/example/shibarichat/ItemActivity.kt
@@ -1,19 +1,47 @@
 package com.example.shibarichat
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shibarichat.Item
+import com.example.shibarichat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ItemActivity : AppCompatActivity(){
     lateinit var data: ArrayList<Item>
+    lateinit var binding: ActivityMainBinding
+    lateinit var auth: FirebaseAuth
+    lateinit var adapter: UserAdapter
+    private val PERMISSION_REQUEST_CODE = 200
+    lateinit var name: String
+
+    lateinit var database: FirebaseDatabase
+    lateinit var myRef: DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item_activity)
+
+        auth = Firebase.auth
+        name = auth.currentUser!!.displayName.toString()
+
         var index = 0
         val button = findViewById<Button>(R.id.button)
         val nameInput = findViewById<EditText>(R.id.name)
@@ -21,16 +49,31 @@ class ItemActivity : AppCompatActivity(){
         val linkInput = findViewById<EditText>(R.id.link)
         val wishRv = findViewById<RecyclerView>(R.id.wishRv)
         data = ArrayList()
+
         var adapter = WishlistAdapter(data)
         wishRv.adapter = adapter
         wishRv.layoutManager = LinearLayoutManager(this)
+
         button.setOnClickListener {
             data.add(Item(nameInput.text.toString(), priceInput.text.toString(), linkInput.text.toString()))
             adapter.notifyItemInserted(index)
 
             index++
 
-            uploadToFirebase()
+            database = Firebase.database
+            myRef = database.getReference("points")
+            myRef.child(myRef.push().key ?: "blablabla")
+                .setValue(Item(nameInput.text.toString(), priceInput.text.toString(), linkInput.text.toString()))
+
+
+            button.isInvisible = true
+            Toast.makeText(
+                this,
+                "Вишлист составлен",
+                Toast.LENGTH_LONG
+            ).show()
+
+
         }
     }
 }
--- QR-Chemistry-main/app/src/main/java/com/example/shibarichat/MainActivity.kt
+++ QR-Chemistry-main/app/src/main/java/com/example/shibarichat/MainActivity.kt
@@ -71,91 +71,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-/*
-        val send = binding.bSend
-        val send2 = binding.bSend2
-        val send3 = binding.bSend3
-        val send4 = binding.bSend4
-        val send5 = binding.bSend5
-        val send6 = binding.editTextTextPersonName5
-
-        send.setOnClickListener {
-
-            ansa = binding.editTextTextPersonName2.text.toString()
-
-            binding.editTextTextPersonName2.hint = "Задание выполнено"
-            binding.editTextTextPersonName2.setText("")
-            binding.editTextTextPersonName2.isFocusable = false
-
-            send.isInvisible = true
-            send2.isVisible = true
-        }
-
-
-
-        send2.setOnClickListener {
-            ansb = binding.bt2.text.toString()
-
-            binding.bt2.hint = "Задание выполнено"
-            binding.bt2.setText("")
-            binding.bt2.isFocusable = false
-
-            send2.isInvisible = true
-            send3.isVisible = true
-        }
-
-        send3.setOnClickListener {
-            ansc = binding.bt.text.toString()
-            binding.bt.hint = "Задание выполнено"
-            binding.bt.setText("")
-            binding.bt.isFocusable = false
-
-
-            send3.isInvisible = true
-            send4.isVisible = true
-        }
-
-
-        send4.setOnClickListener {
-            ansd = binding.editTextTextPersonName3.text.toString()
-            binding.editTextTextPersonName3.hint = "Задание выполнено"
-            binding.editTextTextPersonName3.setText("")
-            binding.editTextTextPersonName3.isFocusable = false
-
-            send4.isInvisible = true
-            send5.isVisible = true
-        }
-
-        send5.setOnClickListener {
-            anse = binding.editTextTextPersonName4.text.toString()
-
-            database = Firebase.database
-            myRef = database.getReference("points")
-            myRef.child(myRef.push().key ?: "blablabla")
-                .setValue(Item(name, ansa, ansb, ansc, ansd, anse, mark))
-
-            binding.editTextTextPersonName4.hint = "Задание выполнено"
-            binding.editTextTextPersonName4.setText("")
-            binding.editTextTextPersonName4.isFocusable = false
-
-            send5.isInvisible = true
-            Toast.makeText(
-                this,
-                "Вишлист составлен",
-                Toast.LENGTH_LONG
-            ).show()
-        }
-
-        send6.setOnClickListener {
-            if (true.also { send5.isInvisible = it } ) {
-                Toast.makeText(
-                    this,
-                    "Генерируем QR-код... ",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }*/
-
         var bScanner: Button? = null
         bScanner = findViewById(R.id.button) as Button
         bScanner?.setOnClickListener {
@@ -175,11 +90,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun scrollRView() {
         var rView = findViewById<RecyclerView>(R.id.rcView)
-    }
-
-    private fun clearEditText() {
-        var edMessage = findViewById<EditText>(R.id.edMessage)
-        edMessage.setText("")
     }
 
     private fun initRcView() = with(binding) {
